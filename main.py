a1f8f79d928efa5b0ee0c4455611ad53356c7d8b
Savitha91 <savi.jc@gmail.com>
2019-11-14 10:05:54 +0000

    validate output column

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt;
 import seaborn as sns;
+df = pd.read_csv("attachment_bikeshare.csv")
+print(df.head(10))
+sns.distplot(df['count']);
+plt.savefig('count_hist.png')
+print("Skewness %f" %df['count'].skew())
+print("Kurtosis %f" %df['count'].kurt())
 
